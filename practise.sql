151b35f8ef8ee2f53de699fca3628915b8db6ed2
J8051 <nathaniel.jessica@yahoo.ca>
2023-11-14 14:15:59 -0500

    writing  querie practise

--- practise.sql
+++ practise.sql
@@ -38,3 +38,113 @@ Q9.  SELECT COUNT(cd) + COUNT(vinyl) AS total, musictype
 Q10.  SELECT artist, title
       FROM music
       WHERE musictype = "rock" AND cd IS NULL;
+
+
+Q11a. SELECT firstname,lastname
+     FROM staff,sales
+     WHERE staff.staffno = sales.staffno AND saledate = "2014-09-23";
+
+Q11b. SELECT staff.firstname, staff.lastname
+     FROM staff
+     JOIN sales
+     ON sales.staffno = staff.staffno
+     WHERE saledate = "2014-09-23";
+
+Q12. SELECT s.lastname Supervisor, e.lastname Employee
+    FROM staff s, staff e
+    WHERE s.staffno = e.supervisorno AND e.lastname = "McLester";
+
+Q13a. SELECT descript Name, salePrice
+     FROM inventory, items
+     WHERE inventory.partno = items.partno AND orderno = "020011";
+
+Q13b. SELECT inventory.descript, inventory.saleprice
+     FROM inventory
+     JOIN items
+     ON items.partno = inventory.partno
+     WHERE items.orderno = "020011";
+
+
+Q14a. SELECT concat('$', FORMAT(SUM(inventory.saleprice * items.qty),2)) 'Total Sales', staff.lastname
+     FROM inventory 
+     JOIN  items, sales, staff
+     WHERE inventory.partno = items.partno
+     AND  items.orderno = sales.orderno 
+     AND staff.staffno = sales.staffno
+     AND staff.staffno =  000008;
+
+
+Q14b. SELECT concat('$', FORMAT(SUM(inventory.saleprice * items.qty),2)) 'Total Sales',  staff.lastname
+     FROM inventory 
+     JOIN  items 
+     ON inventory.partno = items.partno
+     JOIN sales 
+     ON items.orderno = sales.orderno 
+     JOIN staff 
+     ON staff.staffno = sales.staffno
+     WHERE staff.staffno =  000008;
+
+Q15a. SELECT items.orderno, sales.saledate, staff.lastname
+     FROM sales, items, staff 
+     WHERE sales.orderno = items.orderno 
+     AND sales.staffno = staff.staffno  
+     AND month(sales.saledate) = 9; 
+
+Q15b. SELECT items.orderno, sales.saledate, staff.lastname
+     FROM sales 
+     JOIN items 
+     ON sales.orderno = items.orderno
+     JOIN staff 
+     ON sales.staffno = staff.staffno 
+     WHERE month(sales.saledate) = 9; 
+
+Q16a. SELECT  count(sales.staffno), staff.lastname
+     FROM sales, staff 
+     WHERE sales.staffno = staff.staffno
+     AND sales.staffno =  000001;
+
+Q16b. SELECT  count(sales.staffno), staff.lastname
+     FROM sales
+     JOIN staff 
+     ON sales.staffno = staff.staffno
+     WHERE sales.staffno =  000001;
+
+Q17a. SELECT items.orderno,  items.partno, inventory.descript, items.qty, inventory.saleprice, concat('$', format(inventory.saleprice * items.qty,2))  Total
+     FROM inventory, items, sales 
+     WHERE inventory.partno = items.partno
+     AND items.orderno = sales.orderno 
+     AND sales.custno = 100019 ;
+
+Q17b. SELECT items.orderno,  items.partno, inventory.descript,  items.qty, inventory.saleprice, concat('$', format(inventory.saleprice * items.qty,2)) Total
+     FROM inventory 
+     JOIN  items 
+     ON inventory.partno = items.partno
+     JOIN sales 
+     ON items.orderno = sales.orderno 
+     WHERE sales.custno = 100019 ;
+
+Q18a. SELECT sales.custno,inventory.discontinue 
+     FROM inventory ,items,sales
+     WHERE inventory.partno = items.partno
+     AND items.orderno = sales.orderno 
+     AND inventory.discontinue =1; 
+
+Q18b. SELECT sales.custno,inventory.discontinue 
+     FROM inventory 
+     JOIN  items 
+     ON inventory.partno = items.partno
+     JOIN sales 
+     ON items.orderno = sales.orderno 
+     WHERE  inventory.discontinue =1; 
+
+
+
+
+
+
+
+
+
+
+
+
